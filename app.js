fd249d5d35c1193cf23d2fbccf58e3a5f9e545fb
Tina Lam <holambaokhuyen@gmail.com>
2021-12-04 23:56:28 +0700

    menu admin

--- app.js
+++ app.js
@@ -1,41 +1,52 @@
-import express from 'express';
-import morgan from 'morgan';
-import { engine } from 'express-handlebars';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import morgan from "morgan";
+import { engine } from "express-handlebars";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import * as path from "path";
-import { create } from 'express-handlebars';
+import { create } from "express-handlebars";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-app.use(morgan('dev'));
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(morgan("dev"));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.engine('hbs', engine({
-    defaultLayout: 'bs4.hbs',
-    partialsDir: 'views/partials/',
-    extname: '.hbs'
-}));
+app.engine(
+  "hbs",
+  engine({
+    defaultLayout: "bs4.hbs",
+    partialsDir: "views/partials/",
+    extname: ".hbs",
+  })
+);
 
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
 
+app.set("view engine", "hbs");
+app.set("views", "./views");
 
-
-app.set('view engine', 'hbs');
-app.set('views', './views');
-
-app.get('/', function (req, res) {
-    res.render('home');
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
-app.get('/detail', function (req, res) {
-    res.render('ProductView/detail');
+app.get("/detail", function (req, res) {
+  res.render("ProductView/detail");
+});
+
+app.get("/detail", function (req, res) {
+  res.render("ProductView/detail");
+});
+
+app.get("/admin", function (req, res) {
+  res.render("admin/userManagement", { layout: "admin" });
 });
 
 const port = 3000;
 app.listen(port, function () {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
