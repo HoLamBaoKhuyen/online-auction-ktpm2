71fd4edaa184522ed270ceb5bbf544e5dece1862
Tina Lam <holambaokhuyen@gmail.com>
2021-12-27 23:10:09 +0700

    update after merge

--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -4,21 +4,21 @@ import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 export default function (app){
-    app.get("/", function (req, res) {
-        res.render("home");
-      });
+    // app.get("/", function (req, res) {
+    //     res.render("home");
+    //   });
       
-      app.get("/detail", function (req, res) {
-        res.render("ProductView/detail");
-      });
+    //   app.get("/detail", function (req, res) {
+    //     res.render("ProductView/detail");
+    //   });
       
-      app.get("/profile-comment", function (req, res) {
-        res.render("account/profile-comment.hbs");
-      });
+    //   app.get("/profile-comment", function (req, res) {
+    //     res.render("account/profile-comment.hbs");
+    //   });
       
-      app.get('/profile', function (req, res) {
-        res.render('account/profile.hbs');
-      });
+    //   app.get('/profile', function (req, res) {
+    //     res.render('account/profile.hbs');
+    //   });
       
       
       app.get("/admin", function (req, res) {
