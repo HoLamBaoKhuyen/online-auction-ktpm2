c67e9e0694fd22651b3765be9b9c68ab5a34407c
Tina Lam <holambaokhuyen@gmail.com>
2021-12-28 12:52:56 +0700

    thêm class active khi nhấn tab

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -3,16 +3,40 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", function (req, res) {
-  res.render("admin/userManagement", { layout: "admin" });
+  res.render("admin/userManagement", {
+    layout: "admin",
+    isAtAdminUser: true,
+    isAtUserUpdate: false,
+    isAtCategories: false,
+    isAtProducts: false,
+  });
 });
 router.get("/user-update", function (req, res) {
-  res.render("admin/userNeedUpdate", { layout: "admin" });
-});
-router.get("/products", function (req, res) {
-  res.render("admin/productManagement", { layout: "admin" });
+  res.render("admin/userNeedUpdate", {
+    layout: "admin",
+    isAtAdminUser: false,
+    isAtUserUpdate: true,
+    isAtCategories: false,
+    isAtProducts: false,
+  });
 });
 router.get("/categories", function (req, res) {
-  res.render("admin/categories", { layout: "admin" });
+  res.render("admin/categories", {
+    layout: "admin",
+    isAtAdminUser: false,
+    isAtUserUpdate: false,
+    isAtCategories: true,
+    isAtProducts: false,
+  });
+});
+router.get("/products", function (req, res) {
+  res.render("admin/productManagement", {
+    layout: "admin",
+    isAtAdminUser: false,
+    isAtUserUpdate: false,
+    isAtCategories: false,
+    isAtProducts: true,
+  });
 });
 
 export default router;
