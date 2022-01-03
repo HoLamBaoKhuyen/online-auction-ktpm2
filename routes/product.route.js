e241b24e0bb38f3b17812f982126f32b639a1fb7
Nguyễn Thu Thảo Châu <thaochau2201@gmail.com>
2022-01-03 15:44:46 +0700

    update product route

--- routes/product.route.js
+++ routes/product.route.js
@@ -2,6 +2,7 @@ import express from 'express';
 import moment from 'moment';
 import productModel from "../models/product.model.js";
 import userModel from '../models/user.model.js';
+import prodDesModel from '../models/productdes.model.js';
 const router = express.Router();
 
 router.get("/", async function (req, res) {
@@ -430,7 +431,8 @@ router.get('/post',function(req,res){
 router.post('/post', async function(req,res){
   
     const timeEnd =moment(req.body.date +" "+ req.body.time).format("YYYY-MM-DD hh:mm:ss");
-    const user = await userModel.findByEmail(res.locals.authUser.email);
+    const timePosted =moment().format("YYYY-MM-DD hh:mm:ss");
+    const user = await userModel.findByEmail(req.session.authUser.email);
     const product ={
         prodName: req.body.prodName,
         prodType: req.body.prodType,
@@ -438,11 +440,19 @@ router.post('/post', async function(req,res){
         buyNowPrice: req.body.buyNowPrice,
         step: req.body.step,
         timeEnd: timeEnd,
-        timePosted: moment().format("YYYY-MM-DD hh:mm:ss"),
+        timePosted:timePosted ,
         selID: user.uID,
     }
     console.log(product);
    await productModel.addProduct(product);
+   const prod = await productModel.searchForDes(user.uID, req.body.prodName)
+   const productdes = {
+       prodID: prod.prodID,
+       des: req.body.des,
+       modifyTime: timePosted,
+   }
+    console.log(productdes);
+   await prodDesModel.add(productdes);
     res.render('ProductView/postProduct');
 });
 export default router;
