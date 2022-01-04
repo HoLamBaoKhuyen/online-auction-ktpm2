7c7141cce8914cbc321ffd4d6b98fe33c4d8530d
Nguyễn Thu Thảo Châu <thaochau2201@gmail.com>
2022-01-04 14:12:28 +0700

    tách file 2

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,10 +1,8 @@
 import express from "express";
-import moment from "moment";
 import productModel from "../models/product.model.js";
 import profileModel from "../models/profile.model.js";
 
-import userModel from "../models/user.model.js";
-import prodDesModel from "../models/productdes.model.js";
+
 const router = express.Router();
 
 router.get("/", async function (req, res) {
@@ -602,44 +600,5 @@ router.get("/product/sortDate/search", async function (req, res) {
     can_go_prev: +page > 1,
   });
 });
-//------------------------------------ Post-----------------------------------------------
 
-router.get("/postProduct", function (req, res) {
-  if (
-    typeof req.session.auth === "undefined" ||
-    req.session.auth === false ||
-    req.session.authUser.userType !== "seller"
-  ) {
-    res.redirect("/");
-  }
-  res.render("ProductView/postProduct");
-});
-router.post("/postProduct", async function (req, res) {
-  const timeEnd = moment(req.body.date + " " + req.body.time).format(
-    "YYYY-MM-DD hh:mm:ss"
-  );
-  const timePosted = moment().format("YYYY-MM-DD hh:mm:ss");
-  const user = await userModel.findByEmail(req.session.authUser.email);
-  const product = {
-    prodName: req.body.prodName,
-    prodType: req.body.prodType,
-    curPrice: req.body.curPrice,
-    buyNowPrice: req.body.buyNowPrice,
-    step: req.body.step,
-    timeEnd: timeEnd,
-    timePosted: timePosted,
-    selID: user.uID,
-  };
-  console.log(product);
-  await productModel.addProduct(product);
-  const prod = await productModel.searchForDes(user.uID, req.body.prodName);
-  const productdes = {
-    prodID: prod.prodID,
-    des: req.body.des,
-    modifyTime: timePosted,
-  };
-  console.log(productdes);
-  await prodDesModel.add(productdes);
-  res.render("ProductView/postProduct");
-});
 export default router;
