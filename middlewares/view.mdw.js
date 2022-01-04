252446707149a6ae5f7d19322df425f6f8880030
Tina Lam <holambaokhuyen@gmail.com>
2022-01-04 11:49:16 +0700

    config mail

--- middlewares/view.mdw.js
+++ middlewares/view.mdw.js
@@ -43,6 +43,15 @@ export default function (app) {
           if (type === "seller") return true;
           return false;
         },
+        dateToCron(date) {
+          const minutes = date.getMinutes();
+          const hours = date.getHours();
+          const days = date.getDate();
+          const months = date.getMonth() + 1;
+          const dayOfWeek = date.getDay();
+
+          return `${minutes} ${hours} ${days} ${months} ${dayOfWeek}`;
+        },
       },
     })
   );
