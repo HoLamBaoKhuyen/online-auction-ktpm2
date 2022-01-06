ddb67810a0c513177666ffc2ad063f1fc0d479cb
Nguyễn Thu Thảo Châu <thaochau2201@gmail.com>
2022-01-07 00:53:32 +0700

    nâng cấp lên seller

--- routes/account.route.js
+++ routes/account.route.js
@@ -2,7 +2,10 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import Recaptcha from "express-recaptcha";
 import userModel from "../models/user.model.js";
-
+import watchlistModel from "../models/watchlist.model.js";
+import productModel from "../models/product.model.js";
+import auth from "../middlewares/auth.mdw.js";
+import moment from "moment";
 const router = express.Router();
 var recaptcha = new Recaptcha.RecaptchaV2(
   "6LeOKdsdAAAAAO1tRxxSCIoufTKvDLRmuC8Cq7BL",
@@ -93,5 +96,130 @@ router.get("/forgetpassword", function (req, res) {
 });
 router.get("/updatepassword", function (req, res) {
   res.render("Authentication/updatepassword", { layout: "authentication" });
+});
+
+router.get('/upgrade', auth, async function(req,res){
+  const top5Price = await productModel.getTop5HighestPrice();
+  const top5End = await productModel.getTop5End();
+  const top5Bid = await productModel.getTop5HighestBids();
+
+  let newlist1 = productModel.getTimeRemain(top5Price);
+  let newlist2 = productModel.getTimeRemain(top5End);
+  let newlist3 = productModel.getTimeRemain(top5Bid);
+
+  if (req.session.authUser) {
+    if (newlist3) {
+      for (let i = 0; i < newlist3.length; i++) {
+        let proID = newlist3[i].prodID;
+        let wlist = await watchlistModel.findByUidProID(
+          req.session.authUser.uID,
+          proID
+        );
+        if (wlist !== null) {
+          newlist3[i].inWatchlist = 1;
+        }
+      }
+    }
+    if (newlist1) {
+      for (let i = 0; i < newlist1.length; i++) {
+        let proID = newlist1[i].prodID;
+        let wlist = await watchlistModel.findByUidProID(
+          req.session.authUser.uID,
+          proID
+        );
+        if (wlist !== null) {
+          newlist1[i].inWatchlist = 1;
+        }
+      }
+    }
+    if (newlist2) {
+      for (let i = 0; i < newlist2.length; i++) {
+        let proID = newlist2[i].prodID;
+        let wlist = await watchlistModel.findByUidProID(
+          req.session.authUser.uID,
+          proID
+        );
+        if (wlist !== null) {
+          newlist2[i].inWatchlist = 1;
+        }
+      }
+    }
+  }
+  res.render("home",{
+    top5Price: newlist1,
+  top5End: newlist2,
+  top5Bid: newlist3,
+  });
+});
+router.post('/upgrade', auth, async function(req,res){
+  const ret = bcrypt.compareSync(req.body.confirm, req.session.authUser.psword);
+
+  const top5Price = await productModel.getTop5HighestPrice();
+  const top5End = await productModel.getTop5End();
+  const top5Bid = await productModel.getTop5HighestBids();
+
+  let newlist1 = productModel.getTimeRemain(top5Price);
+  let newlist2 = productModel.getTimeRemain(top5End);
+  let newlist3 = productModel.getTimeRemain(top5Bid);
+
+  if (req.session.authUser) {
+    if (newlist3) {
+      for (let i = 0; i < newlist3.length; i++) {
+        let proID = newlist3[i].prodID;
+        let wlist = await watchlistModel.findByUidProID(
+          req.session.authUser.uID,
+          proID
+        );
+        if (wlist !== null) {
+          newlist3[i].inWatchlist = 1;
+        }
+      }
+    }
+    if (newlist1) {
+      for (let i = 0; i < newlist1.length; i++) {
+        let proID = newlist1[i].prodID;
+        let wlist = await watchlistModel.findByUidProID(
+          req.session.authUser.uID,
+          proID
+        );
+        if (wlist !== null) {
+          newlist1[i].inWatchlist = 1;
+        }
+      }
+    }
+    if (newlist2) {
+      for (let i = 0; i < newlist2.length; i++) {
+        let proID = newlist2[i].prodID;
+        let wlist = await watchlistModel.findByUidProID(
+          req.session.authUser.uID,
+          proID
+        );
+        if (wlist !== null) {
+          newlist2[i].inWatchlist = 1;
+        }
+      }
+    }
+  }
+  if (ret=== true){
+    const request ={
+      bidID:req.session.authUser.uID,
+      reqTime:moment().format("YYYY-MM-DD hh:mm:ss"),
+    }
+    await userModel.requestUpgrade(request);
+    res.render("home",{
+      mes:"Xin nâng cấp thành công",
+      top5Price: newlist1,
+    top5End: newlist2,
+    top5Bid: newlist3,
+    });
+  }
+  else{
+    res.render("home",{
+      warn:"Xin nâng cấp thất bại",
+      top5Price: newlist1,
+    top5End: newlist2,
+    top5Bid: newlist3,
+    });
+  }
 });
 export default router;
