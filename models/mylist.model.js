43af75f609b04360130d7cf03bd7ef7701ab8260
Nguyễn Thu Thảo Châu <thaochau2201@gmail.com>
2022-01-07 21:42:21 +0700

    danh sách sản phẩm đã có người thắng

--- models/mylist.model.js
+++ models/mylist.model.js
@@ -11,7 +11,7 @@ export default {
     return raw[0].length;
   },
   async getParticipatingProdPage(limit, offset, id) {
-    const sql = `select p.*, concat('***** ',u.firstname) AS nameofUser, par.*
+    const sql = `select p.*, concat('***** ',u.firstname) AS nameofUser, par.*, count(par.prodID) AS CountBids
                         from products p
                         left join users u on p.highestBidID = u.UID
                         left join participate par on par.prodID = p.prodID
@@ -48,9 +48,33 @@ export default {
     return raw[0].length;
   },
   async getPostProdPage(limit, offset, id) {
-    const sql = `select p.* from products p
-    where timeEnd > now() and p.selID = ${id} limit ${limit} offset ${offset};`;
+    // const sql = `select p.* from products p
+    // where timeEnd > now() and p.selID = ${id} limit ${limit} offset ${offset};`;
+
+    const sql =`select p.* , concat('***** ',u.firstname) AS nameofUser, count(pa.prodID) AS CountBids
+    from products p left join users u  on p.highestBidID = u.uID
+    left join participate pa on p.prodID = pa.prodID
+    where timeEnd > now() and p.selID = ${id} 
+    group by p.prodID limit ${limit} offset ${offset};` ;
     const raw = await db.raw(sql);
     return raw[0];
+  },
+
+  async countEndedProd(id){
+      const sql =`select p.*
+      from products p 
+      where p.prodID=`+id+` and p.timeEnd < now();`;
+      const ret= await db.raw(sql);
+      return ret[0].length;
+  },
+  async getEndedProdPage(limit,offset,id){
+      const sql = `select p.* , concat('***** ',u.firstname) AS nameofUser, count(pa.prodID) AS CountBids
+      from products p left join users u  on p.highestBidID = u.uID
+      left join participate pa on p.prodID = pa.prodID
+      where   p.timeEnd < now() and p.highestBidID is not null
+      and p.selID = ${id}
+      group by p.prodID limit ${limit} offset ${offset};`;
+      const raw = await db.raw(sql);
+      return raw[0];
   },
 };
