e0b24ce75a50a3ee98c72b224386e6ac1ea8bb99
Tina Lam <holambaokhuyen@gmail.com>
2022-01-07 20:33:52 +0700

    update ui form đăng ký

--- middlewares/auth.mdw.js
+++ middlewares/auth.mdw.js
@@ -1,9 +1,8 @@
-
-export default function auth(req,res,next){
-  console.log(req.session.auth);
-    if (typeof(req.session.auth) === 'undefined'||req.session.auth===false){
-      req.session.retUrl=req.originalUrl;
-      return res.redirect('/account/login');
-    }
-    next();
-};
+export default function auth(req, res, next) {
+  // console.log(req.session.auth);
+  if (typeof req.session.auth === "undefined" || req.session.auth === false) {
+    req.session.retUrl = req.originalUrl;
+    return res.redirect("/account/login");
+  }
+  next();
+}
