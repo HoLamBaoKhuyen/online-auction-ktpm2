9be7c5e3e4e1442d14e079fd6e660dfd5ab64868
Tina Lam <holambaokhuyen@gmail.com>
2022-01-11 16:28:35 +0700

    change port

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ import db from "./utils/db.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 // app.set('trust proxy', 1);
 app.use(morgan("dev"));
