6c9baa8a84651c20330656ae1b6f781232e8bd27
Tina Lam <63384365+HoLamBaoKhuyen@users.noreply.github.com>
2022-01-11 14:08:23 +0700

    Update session.mdw.js

--- middlewares/session.mdw.js
+++ middlewares/session.mdw.js
@@ -3,9 +3,9 @@ export default function (app){
     
 app.set('trust proxy', 1) // trust first proxy
 app.use(session({
-  secret: 'SECRET_KEY',
+  secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   cookie: { 
     //secure: true 
   }
