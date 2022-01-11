068491f72bb69c328f1212d19b318b1eef467df9
Tina Lam <holambaokhuyen@gmail.com>
2022-01-11 17:41:19 +0700

    Update session.mdw.js

--- middlewares/session.mdw.js
+++ middlewares/session.mdw.js
@@ -2,18 +2,27 @@ import session from "express-session";
 import mySqlSessionStore from "express-mysql-session";
 import { connectionInfo } from "../utils/db.js";
 export default function (app) {
-  const MySqlSession = mySqlSessionStore(session);
+  // app.set("trust proxy", 1); // trust first proxy
+  // app.use(
+  //   session({
+  //     secret: process.env.SESSION_SECRET,
+  //     resave: false,
+  //     saveUninitialized: false,
+  //     store: new MySqlSession({
+  //       ...connectionInfo,
+  //       expiration: COOKIE_MAX_AGE,
+  //     }),
+  //   })
+  // );
 
-  app.set("trust proxy", 1); // trust first proxy
+  var sessionStore = new MySQLStore(connectionInfo);
+  app.set("trust proxy", 1);
   app.use(
     session({
       secret: process.env.SESSION_SECRET,
+      store: sessionStore,
       resave: false,
       saveUninitialized: false,
-      store: new MySqlSession({
-        ...connectionInfo,
-        expiration: COOKIE_MAX_AGE,
-      }),
     })
   );
 }
